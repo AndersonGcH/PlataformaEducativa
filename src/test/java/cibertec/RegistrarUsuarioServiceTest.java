922ba6bf11388e48c34a781b41ca17acafe57a6c
AndersonGcH <andersoncutipa113@gmail.com>
2026-03-30 00:43:26 -0500

    test 1 (red): registro exitoso con datos válidos

--- src/test/java/cibertec/RegistrarUsuarioServiceTest.java
+++ src/test/java/cibertec/RegistrarUsuarioServiceTest.java
@@ -1,7 +1,14 @@
 package cibertec;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RegistrarUsuarioServiceTest {
-
+    @Test
+    void deberiaRegistrarUsuarioCuandoTodosLosDatosSonValidos() {
+        RegistrarUsuarioService service = new RegistrarUsuarioService();
+        String resultado = service.registrarUsuario("user123", "clave1234", "mail@edu.com", 20);
+        assertEquals("El usuario ha sido registrado correctamente", resultado);
+    }
 }
