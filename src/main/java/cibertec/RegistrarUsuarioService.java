9a1d7351fec026726d17c5ffd628836f3654e020
AndersonGcH <andersoncutipa113@gmail.com>
2026-03-30 01:04:11 -0500

    test 2(green):implementacion minima

--- src/main/java/cibertec/RegistrarUsuarioService.java
+++ src/main/java/cibertec/RegistrarUsuarioService.java
@@ -1,7 +1,15 @@
 package cibertec;
 
 public class RegistrarUsuarioService {
-    public String registrarUsuario(String usuario, String contrasenia, String correo, int edad) {
+    public String registrarUsuario(String usuario, String contrasenia, String correo, Integer edad) {
+
+        if (usuario == null || usuario.trim().isEmpty()
+                || contrasenia == null || contrasenia.trim().isEmpty()
+                || correo == null || correo.trim().isEmpty()
+                || edad == null) {
+            return "Debe completar todos los campos requeridos";
+        }
         return "El usuario ha sido registrado correctamente";
+
     }
 }
