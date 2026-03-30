6032d159e2d22c3d068a926731ef99241db3eb50
AndersonGcH <andersoncutipa113@gmail.com>
2026-03-30 00:54:19 -0500

    test 1 (blue): nombres consistentes

--- src/main/java/cibertec/RegistrarUsuarioService.java
+++ src/main/java/cibertec/RegistrarUsuarioService.java
@@ -1,7 +1,7 @@
 package cibertec;
 
 public class RegistrarUsuarioService {
-    public String registrarUsuario(String username, String password, String email, Integer edad) {
+    public String registrarUsuario(String usuario, String contrasenia, String correo, int edad) {
         return "El usuario ha sido registrado correctamente";
     }
 }
