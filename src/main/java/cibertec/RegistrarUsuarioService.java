cfd7bc8f6c832541b101377699605c3528d262e6
AndersonGcH <andersoncutipa113@gmail.com>
2026-03-30 00:47:47 -0500

    test 1 (green): implementación minima para registro exitoso

--- src/main/java/cibertec/RegistrarUsuarioService.java
+++ src/main/java/cibertec/RegistrarUsuarioService.java
@@ -1,5 +1,7 @@
 package cibertec;
 
 public class RegistrarUsuarioService {
-
+    public String registrarUsuario(String username, String password, String email, Integer edad) {
+        return "El usuario ha sido registrado correctamente";
+    }
 }
